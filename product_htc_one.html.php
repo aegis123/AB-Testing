6294a8d32c9515e742b5b70789090c90f02914b3
aegis123 <dylan.drost@gmail.com>
2013-05-29 16:59:53 +0200

    fixed all links

--- product_htc_one.html.php
+++ product_htc_one.html.php
@@ -13,17 +13,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="brand" href="index.html">A/B testing webshop</a>
+            <a class="brand" href="index.html.php">A/B testing webshop</a>
 
             <div class="nav-collapse collapse">
                 <ul class="nav">
-                    <li><a href="index.html"><i class="icon-home icon-white"></i> Home</a></li>
-                    <li><a href="winkelmandje.html"><i class="icon-shopping-cart icon-white"></i> Winkelmandje</a></li>
+                    <li><a href="index.html.php"><i class="icon-home icon-white"></i> Home</a></li>
+                    <li><a href="winkelmandje.html.php"><i class="icon-shopping-cart icon-white"></i> Winkelmandje</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Producten <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li class="active"><a href="product_htc_one.html">HTC One</a></li>
-                            <li><a href="product_iphone.html">Iphone 5</a></li>
+                            <li class="active"><a href="product_htc_one.html.php">HTC One</a></li>
+                            <li><a href="product_iphone.html.php">Iphone 5</a></li>
                         </ul>
                     </li>
                     <li><a href="#contact">Contact</a></li>
@@ -52,7 +52,7 @@
                 zodat je bij het kijken van een video kunt genieten van echt stereo geluid - uiteraard met HTC's bekende Beats Audio technologie.
             </p>
             <br/><br/><br />
-            <p><a class="btn btn-large btn-success" href="winkelmandje.html" onclick="addProductToCart('iphone', 600, 1)">KOPEN !!!</a></p>
+            <p><a class="btn btn-large btn-success" href="winkelmandje.html.php" onclick="addProductToCart('iphone', 600, 1)">KOPEN !!!</a></p>
         </div>
     </div>
     <div class="row-fluid">
