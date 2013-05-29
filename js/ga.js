f832ae7aa5c568f4aea134f44f347e4d7eb93ffd
aegis123 <dylan.drost@gmail.com>
2013-05-29 11:48:36 +0200

    added some text to page

---
+++ js/ga.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: dylan
+ * Date: 29-5-13
+ * Time: 11:34
+ * To change this template use File | Settings | File Templates.
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-41294141-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
