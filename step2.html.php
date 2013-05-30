a69ba383b7aeadc45d94ed36f063cd3afd9c3c82
aegis123 <dylan.drost@gmail.com>
2013-05-30 14:58:25 +0200

    adress

--- step2.html.php
+++ step2.html.php
@@ -1,30 +1,33 @@
 <?php ob_start();
-    include 'step2.php';
+include 'step2.php';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Bevestiging</title>
     <?php include 'header.php' ?>
 </head>
 <body>
-    <?php include 'menu.php' ?>
+<?php include 'menu.php' ?>
+<div class="container">
+    <div class="row-fluid">
+        <div class="span12">
+            <h1>Bevestiging</h1>
 
-    <div class="container">
-        <div class="span6">
-        <table class="table" >
-            <thead>
-                <tr><th>Bevestiging</th></tr>
-            </thead>
-            <tbody>
-                <tr><th>Straat</th>
-                    <td>xxx</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <button type="button" class="btn btn-success btn-large"><i class="icon-ok icon-white"></i> bevestig aankoop</button>
+            <div class="span6">
+                <h2>Verzend adres</h2>
+                <address>
+                    <strong><?= $naam ?></strong><br/>
+                    <?= $straat . " " . $huisnr ?><br/>
+                    <?= $postcode . " " . $stad ?><br/>
+                </address>
+            </div>
         </div>
+
+        <a type="button" class="btn btn-success btn-large" href="thankyou.html.php"><i
+                class="icon-ok icon-white"></i> bevestig aankoop</a>
+
     </div>
+</div>
 </body>
 </html>
