9c3a2b91be2b57e13eaf883fa33b35086694766f
aegis123 <dylan.drost@gmail.com>
2013-06-03 14:05:39 +0200

    added contact page

---
+++ contact.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thankyou A/B testing</title>
+    <?php include 'header.php' ?>
+</head>
+<body>
+<?php include 'menu.php' ?>
+<div class="container">
+    <!-- Main hero unit for a primary marketing message or call to action -->
+    <div class="hero-unit">
+        <h1>Contact</h1>
+        <address>
+            <strong>Hogeschool Utrecht</strong><br />
+            Nijenoord 1<br />
+            3552 AS Utrecht
+            <abbr title="Telefoon">t:</abbr> (088) 481 8283
+            <a href="mailto:info@hu.nl">info@hu.nl</a>
+        </address>
+    </div>
+</div>
+</body>
+</html>
